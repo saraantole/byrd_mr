5348d1a9400405003aa4c2b9461e82072cf9b242
saraantole <sara.antole@gmail.com>
2022-03-26 16:44:11 +0100

    created nav component + sidemenu

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,42 @@
 import { Link, useLocation } from 'react-router-dom'
 import { Header } from './styles'
+import defaultLogo from '../../images/Logo.png'
+import FacebookIcon from '../../icons/FacebookIcon'
+import TwitterIcon from '../../icons/TwitterIcon'
+import HamburgerMenu from '../../icons/HamburgerMenu'
 
 const Navbar = () => {
     const url = useLocation()
 
-    console.log(url.pathname)
     return (
-        <Header>
-            <Link to='/contact'>Contact</Link>
-            <Link to='/tutorials'>Tutorials</Link>
+        <Header path={url.pathname}>
+            <nav>
+                {url.pathname === '/'
+                    ? <Link to=''>Store</Link>
+                    : <Link to=''>
+                        <FacebookIcon />
+                    </Link>
+                }
+                {url.pathname === '/'
+                    ? <Link to='/tutorials'>Tutorials</Link>
+                    : <Link to=''>
+                        <TwitterIcon />
+                    </Link>
+                }
+
+            </nav>
+            <Link to='/'>
+                <img width={url.pathname === '/' ? 216 : 176} src={defaultLogo} />
+            </Link>
+            <nav>
+                {url.pathname === '/' && window.innerWidth > 786
+                    ? <>
+                        <Link to=''>Blog</Link>
+                        <Link to='/contact'>Contact</Link>
+                    </>
+                    : <HamburgerMenu />
+                }
+            </nav>
         </Header>
     )
 }
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,6 +1,28 @@
 import styled from 'styled-components'
 
-export const Header = styled.main`
+export const Header = styled.header`
     position: fixed;
     width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: ${({ path }) => path === '/' ? '60px 135px' : '40px 33px'};
+
+    a {
+        font-weight: 500;
+        text-transform: uppercase;
+        font-size: 14px;
+        line-height: 15px;
+        text-align: center;
+        letter-spacing: 1px;
+        color: ${({ theme }) => theme.colors.black};
+
+        &:nth-child(odd) {
+            padding-right: 55px;
+        }
+
+        svg {
+            opacity: 20%;
+        }
+    }
 `
