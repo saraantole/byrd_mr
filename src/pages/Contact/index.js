a09282271aa2263883685c35b3f4515056f40d58
saraantole <sara.antole@gmail.com>
2022-03-27 19:04:11 +0200

    adding form elements and styles + added logic

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,9 +1,40 @@
 import { Container } from './styles'
+import ArrowIcon from '../../icons/ArrowIcon'
+import { useState } from 'react'
 
 const Contact = () => {
+    const [inputs, setInputs] = useState({ name: '', email: '', message: '' })
+
+    const handleInputs = event => {
+        const input = event.target.name
+        const value = event.target.value
+        setInputs(prevInputs => ({ ...prevInputs, [input]: value }))
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        alert(`Hi, ${inputs.name}. Your message was submitted! You can check your console`)
+        console.log(inputs)
+        // TODO: handle form submission
+    }
+
     return (
         <Container>
-
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Full Name
+                    <input onChange={handleInputs} name='name' value={inputs.name} type='text' placeholder='Enter your full name' />
+                </label>
+                <label>
+                    Email
+                    <input onChange={handleInputs} name='email' value={inputs.email} type='email' placeholder='Enter your email' />
+                </label>
+                <label>
+                    Message
+                    <textarea onChange={handleInputs} name='message' value={inputs.message} placeholder='What are your plans?' />
+                </label>
+                <button type='submit'>Send Message <ArrowIcon /></button>
+            </form>
         </Container>
     )
 }
